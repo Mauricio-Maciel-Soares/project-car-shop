c4e1703cca875ba476ba5e596d997795e6aadac2
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-08-01 15:38:18 -0300

    feat: adiciona carMock na pasta tests

---
+++ src/tests/mocks/carMock.ts
@@ -0,0 +1,15 @@
+import { ICar } from '../../interfaces/ICar';
+
+const carMock: ICar = {
+	model: 'Fusca',
+  year:1979,
+  color: 'Azul',
+  status: true,
+  buyValue: 15000,
+  seatsQty: 4,
+  doorsQty: 2,
+};
+
+export {
+	carMock,
+};
